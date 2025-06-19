7f311cdc7ac8d2e66e13b7f6b85aa390016b0917
nganesh <nganesh@iu.edu>
2025-06-18 19:20:52 -0700

    tuple operations

---
+++ tuple operations.py
@@ -0,0 +1,15 @@
+tuplex = ("tuple", "false", 1.3, 7)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = ("greece", "italy", "spain", "india", "switzerland", "india", "greece", "greece")
+print(tuple1.count("greece"))
+
+tuple2 = (5, 10, 15, 20, 25)
+slicing = tuple2[2:4]
+print(slicing)
+
+slicing = tuple2[:5]
+print(slicing)
